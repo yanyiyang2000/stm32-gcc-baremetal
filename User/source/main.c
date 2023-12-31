4dc6f41fa701570c21c9ffb21215eff6f6ae0113
Yiyang Yan <szyyy2000@gmail.com>
2023-12-31 00:09:36 -0500

    update

--- User/source/main.c
+++ User/source/main.c
@@ -8,9 +8,19 @@
 
 
 int main() {
+	/* Configure MSI to around 48 MHz */
 	MSI_config();
+
+	/* Configure SysTick interrupt to be triggered every 1 ms */
 	SysTick_config();
+
+	/* Update the SystemCoreClock variable */
+	SystemCoreClockUpdate();
+
+	/* Configure GPIO PA5 to control LED */
 	GPIO_PA5_config();
+
+	/* Configure GPIO PA2 and PA3 as UART receiver and transmitter */
 	GPIO_PA2_config();
 	GPIO_PA3_config();
 }
